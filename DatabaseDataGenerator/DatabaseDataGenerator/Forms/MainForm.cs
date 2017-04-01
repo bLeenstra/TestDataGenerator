ed78cb1463506a51d2a951b1824849789e0031a5
bLeenstra <brendan.leenstra@gmail.com>
2017-04-01 15:39:05 +1100

    added forms

---
+++ DatabaseDataGenerator/DatabaseDataGenerator/Forms/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DatabaseDataGenerator.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
